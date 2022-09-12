45064ad411142ea7b97ec2d87bb89e6d1ddb833a
quyetvu129 <74765071+quyetvu129@users.noreply.github.com>
2022-09-12 22:23:04 +0700

    updating configure default

--- eservice/src/main/java/com/example/eservice/EserviceApplication.java
+++ eservice/src/main/java/com/example/eservice/EserviceApplication.java
@@ -2,12 +2,15 @@ package com.example.eservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
 public class EserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EserviceApplication.class, args);
+    }
 
 }
---
+++ eservice/src/main/java/com/example/eservice/api/NewAPI.java
@@ -0,0 +1,14 @@
+package com.example.eservice.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewAPI {
+
+    @GetMapping("/test")
+    public String testAPI() {
+        return "success";
+    }
+}
